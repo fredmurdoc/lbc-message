88e04cb52a4adf37c3d1b22e054e468c172e8f16
Frederic GONTIER <fredgont@yahoo.fr>
2022-03-03 23:12:22 +0100

    ajout setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='LbcMessage',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Frederic Gontier',
+      author_email='fredgont@gmail.com',
+      packages=['.'],
+     )
