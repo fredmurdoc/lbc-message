0eed60437c21bf75fbbb45be17477ecb38f193c2
Frederic GONTIER <fredgont@yahoo.fr>
2022-03-24 22:20:41 +0100

    mise en place du scritp de mise à jour

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 python $(pwd)/main.py
 python $(pwd)/extract_items.py
 python $(pwd)/enrich_items.py
+python $(pwd)/etat_annonces.py
 cp items.json ../django_projects/rechercheMaison/data/maisons.json
 cd ../django_projects/rechercheMaison
 python manage.py runscript import_maisons
